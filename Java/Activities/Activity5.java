60f6de1e64202999ea85437c39f6c891883ae06c
AnuBalan08 <anu.balan@ibm.com>
2024-12-19 12:21:04 +0530

    Add files via upload

---
+++ Java/Activities/Activity5.java
@@ -0,0 +1,10 @@
+
+public class Activity5 {
+
+	public static void main(String[] args) {
+		String title = "Wings of Fire";
+		MyBook newNovel = new MyBook();
+		newNovel.setTitle(title);
+		System.out.println(newNovel.getTitle());
+	}
+}
---
+++ Java/Activities/Book.java
@@ -0,0 +1,12 @@
+
+abstract class Book {
+	
+	String title; 
+	
+	abstract void setTitle(String title);
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+}
